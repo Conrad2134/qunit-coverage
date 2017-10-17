fe3fdaeb3e704f3a37ae4a15835f0ce9dc1dbba7
Conrad2134 <connor24@netins.net>
2017-10-17 14:46:35 -0500

    forgot to save

--- src/index.js
+++ src/index.js
@@ -9,92 +9,79 @@ module.exports = function executeTestRunner(
 	options = {},
 	callback = () => {}
 ) {
-	const runner = "./runner.js";
-	const absolutePath = path.resolve(filePath);
-	const isAbsolutePath = absolutePath.indexOf(filePath) !== -1;
-	const childArgs = [];
+		const runner = "./runner.js";
+		const absolutePath = path.resolve(filePath);
+		const isAbsolutePath = absolutePath.indexOf(filePath) !== -1;
+		const childArgs = [];
 
-	//	options:
-	//		verbose
-	//		chrome-options (similar to phantomjs-options)
-	//		timeout
-	//		page?
-	//		which coverage formats to output
+		//	options:
+		//		`coverage: { output: "...", formats: ["json", ...] }` OR `coverage: false` (does it by default to cwd and json)
+		//		verbose
+		//		chrome-options (similar to phantomjs-options)
+		//		timeout
+		//		page?
 
-	/* console.log(
+		/* console.log(
 		"file:///" + path.join(process.cwd(), filePath).replace(/\\/g, "/") // TODO: format
 	); */
 
-	// Setting our timeout in case everything below takes too long
-	setTimeout(() => {
-		console.log();
-		console.log(chalk.red("TIMEOUT, SUCKER"));
-		console.log();
-		process.exit(1);
-	}, options.timeout || 5000);
-
-	(async () => {
-		console.log(
-			"Testing",
-			chalk.magenta(path.relative(process.cwd(), filePath))
-		);
-
-		const browser = await puppeteer.launch({ headless: true }); // defaults to true, not needed
-		const page = await browser.newPage();
-
-		await page.exposeFunction("report", async response => {
-			const coverage = await page.evaluate(() => __coverage__);
-
-			console.dir(response);
-
-			const collector = new istanbul.Collector();
-			const reporter = new istanbul.Reporter(false, options.output);
-
-			collector.add(coverage);
-
-			reporter.addAll(["lcovonly", "html", "text-summary", "json"]);
-			reporter.write(collector, true, () => {
-				console.log();
-				console.log(`Coverage written to ${chalk.magenta(options.output)}`);
-			});
-
+		// Setting our timeout in case everything below takes too long
+		setTimeout(() => {
 			console.log();
-			console.log(
-				"Took " +
-					response.runtime +
-					"ms to run " +
-					response.total +
-					" tests. " +
-					response.passed +
-					" passed, " +
-					response.failed +
-					" failed."
-			);
+			console.log(chalk.red("TIMEOUT, SUCKER"));
+			console.log();
+			process.exit(1);
+		}, options.timeout || 5000);
 
-			await browser.close();
-		});
+		(async () => {
+			console.log("Testing", chalk.magenta(path.relative(process.cwd(), filePath)));
 
-		await page.on("load", async () => {
-			const qunitMissing = await page.evaluate(() => {
-				return typeof QUnit === "undefined" || !QUnit;
+			const browser = await puppeteer.launch({ headless: true }); // defaults to true, not needed
+			const page = await browser.newPage();
+
+			await page.exposeFunction("report", async response => {
+				const coverage = await page.evaluate(() => __coverage__);
+
+				console.dir(response);
+
+				const collector = new istanbul.Collector();
+				const reporter = new istanbul.Reporter(false, options.output);
+
+				collector.add(coverage);
+
+				reporter.addAll(["lcovonly", "html", "text-summary", "json"]);
+				reporter.write(collector, true, () => {
+					console.log();
+					console.log(`Coverage written to ${chalk.magenta(options.output)}`);
+				});
+
+				console.log();
+				console.log("Took " + response.runtime + "ms to run " + response.total + " tests. " + response.passed + " passed, " + response.failed + " failed.");
+
+				await browser.close();
 			});
 
-			if (qunitMissing) {
-				console.log();
-				console.log(chalk.red("QUnit seems to be missing... :/")); // TODO: message
-				console.log();
+			await page.on("load", async () => {
+				const qunitMissing = await page.evaluate(() => {
+					return typeof QUnit === "undefined" || !QUnit;
+				});
 
-				process.exit(1);
-			}
+				if (qunitMissing) {
+					console.log();
+					console.log(chalk.red("QUnit seems to be missing... :/")); // TODO: message
+					console.log();
 
-			await page.evaluate(() => {
-				QUnit.done(window.report);
+					process.exit(1);
+				}
+
+				await page.evaluate(() => {
+					QUnit.done(window.report);
+				});
 			});
-		});
 
-		// Navigate to our test file
-		await page.goto(
-			"file:///" + path.join(process.cwd(), filePath).replace(/\\/, "/")
-		);
-	})();
-};
+			// Navigate to our test file
+			await page.goto("file:///" + path
+						.join(process.cwd(), filePath)
+						.replace(/\\/, "/"));
+		})();
+	};
